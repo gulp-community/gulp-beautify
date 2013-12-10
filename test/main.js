300af861dee42170bce44a2c1e2ba56509d30ab7
Contra <contra@maricopa.edu>
2013-12-10 03:17:38 -0700

    update tests for 3.0 and set deps better

--- test/main.js
+++ test/main.js
@@ -1,28 +1,30 @@
 var beautify = require('../');
 var should = require('should');
 var nbeautify = require('node-beautify');
+var gutil = require('gulp-util');
 require('mocha');
 
 describe('gulp-beautify', function() {
   describe('beautify()', function() {
     it('should concat two files', function(done) {
       var stream = beautify({indentSize: 2});
-      var fakeFile = {
+      var fakeFile = new gutil.File({
         path: "/home/contra/test/file.js",
-        shortened: "file.js",
+        base: "/home/contra/test/",
+        cwd: "/home/contra/",
         contents: new Buffer("function test(){console.log('test');}")
-      };
+      });
 
       var expected = nbeautify.beautifyJs(String(fakeFile.contents), {indentSize: 2});
       stream.on('error', done);
       stream.on('data', function(newFile){
         should.exist(newFile);
         should.exist(newFile.path);
-        should.exist(newFile.shortened);
+        should.exist(newFile.relative);
         should.exist(newFile.contents);
 
         newFile.path.should.equal("/home/contra/test/file.js");
-        newFile.shortened.should.equal("file.js");
+        newFile.relative.should.equal("file.js");
         String(newFile.contents).should.equal(expected);
         done();
       });
