ca343ca13a402e6353ce06084a7e7044e1df3d9b
Hiswe <hiswehalya@gmail.com>
2018-10-14 12:02:59 +0700

    BEAUTIFY – add support of html files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+var path = require('path');
 var through = require('through2');
-var beautify = require('js-beautify').js_beautify;
+var beautify = require('js-beautify');
 var RcLoader = require('rcloader');
 
 module.exports = function(opts) {
@@ -10,9 +11,14 @@ module.exports = function(opts) {
     if (file.isStream()) return cb(new Error('gulp-beautify: Streaming not supported'));
     rcLoader.for(file.path, function(err, opts) {
       if (err) return cb(err);
-
+      var extname = path.extname(file.path);
+      var parser = /^\.html?$/.test(extname)
+        ? beautify.html
+        : /^\.css$/.test(extname)
+          ? beautify.css
+          : beautify.js;
       var str = file.contents.toString('utf8');
-      file.contents = new Buffer.from(beautify(str, opts));
+      file.contents = new Buffer.from(parser(str, opts));
       cb(null, file);
     });
   }
--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var File = require('vinyl');
 require('mocha');
 
 describe('gulp-beautify', function() {
-  describe('beautify()', function() {
+  describe('beautify() – javascript', function() {
     it('should concat two files', function(done) {
       var stream = beautify({ indent_size: 2 });
       var fakeFile = new File({
@@ -24,6 +24,92 @@ describe('gulp-beautify', function() {
 
         newFile.path.should.equal('/home/contra/test/file.js');
         newFile.relative.should.equal('file.js');
+        String(newFile.contents).should.equal(expected);
+        done();
+      });
+      stream.write(fakeFile);
+    });
+  });
+
+  describe('beautify() – html', function() {
+    var getHtmlFile = function(ext) {
+      return new File({
+        path: `/home/contra/test/file.${ext}`,
+        base: '/home/contra/test/',
+        cwd: '/home/contra/',
+        contents: new Buffer.from('<html><body><h1>foo</h1><h2>bar</h2></body></html>')
+      });
+    };
+    var expected = `<html>
+
+<body>
+  <h1>foo</h1>
+  <h2>bar</h2>
+</body>
+
+</html>`;
+
+    it('should beautify .html', function(done) {
+      var stream = beautify({ indent_size: 2 });
+      var fakeFile = getHtmlFile(`html`);
+
+      stream.on('error', done);
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+
+        newFile.path.should.equal('/home/contra/test/file.html');
+        newFile.relative.should.equal('file.html');
+        String(newFile.contents).should.equal(expected);
+        done();
+      });
+      stream.write(fakeFile);
+    });
+
+    it('should beautify .htm', function(done) {
+      var stream = beautify({ indent_size: 2 });
+      var fakeFile = getHtmlFile(`htm`);
+
+      stream.on('error', done);
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+
+        newFile.path.should.equal('/home/contra/test/file.htm');
+        newFile.relative.should.equal('file.htm');
+        String(newFile.contents).should.equal(expected);
+        done();
+      });
+      stream.write(fakeFile);
+    });
+  });
+
+  describe('beautify() – css', function() {
+    it('should beautify .css', function(done) {
+      var stream = beautify({ indent_size: 2 });
+      var fakeFile = new File({
+        path: '/home/contra/test/file.css',
+        base: '/home/contra/test/',
+        cwd: '/home/contra/',
+        contents: new Buffer.from(`.foo { color: red; }`)
+      });
+
+      var expected = `.foo {
+  color: red;
+}`;
+      stream.on('error', done);
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+
+        newFile.path.should.equal('/home/contra/test/file.css');
+        newFile.relative.should.equal('file.css');
         String(newFile.contents).should.equal(expected);
         done();
       });
