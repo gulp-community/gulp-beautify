4a99eb0bbab7efb275b692402b38e5a3aea75114
Hiswe <hiswehalya@gmail.com>
2018-12-04 15:08:43 +0700

    TESTS – add for Node 4

--- test/index.js
+++ test/index.js
@@ -3,18 +3,24 @@ var should = require('should');
 var File = require('vinyl');
 require('mocha');
 
+function createBuffer(string) {
+  return Buffer.from ? new Buffer.from(string) : new Buffer(string);
+}
+
 describe('gulp-beautify', function() {
   describe('beautify() – javascript', function() {
+    var jsSource = createBuffer('function test(){console.log("test");}');
+    var jsExpected = 'function test() {\n  console.log("test");\n}';
+
     it('should concat two files', function(done) {
       var stream = beautify({ indent_size: 2 });
       var fakeFile = new File({
         path: '/home/contra/test/file.js',
         base: '/home/contra/test/',
         cwd: '/home/contra/',
-        contents: new Buffer.from('function test(){console.log("test");}')
+        contents: jsSource
       });
 
-      var expected = 'function test() {\n  console.log("test");\n}';
       stream.on('error', done);
       stream.on('data', function(newFile) {
         should.exist(newFile);
@@ -24,7 +30,7 @@ describe('gulp-beautify', function() {
 
         newFile.path.should.equal('/home/contra/test/file.js');
         newFile.relative.should.equal('file.js');
-        String(newFile.contents).should.equal(expected);
+        String(newFile.contents).should.equal(jsExpected);
         done();
       });
       stream.write(fakeFile);
@@ -36,10 +42,9 @@ describe('gulp-beautify', function() {
         path: '/home/contra/test/file.js',
         base: '/home/contra/test/',
         cwd: '/home/contra/',
-        contents: new Buffer.from('function test(){console.log("test");}')
+        contents: jsSource
       });
 
-      var expected = 'function test() {\n  console.log("test");\n}';
       stream.on('error', done);
       stream.on('data', function(newFile) {
         should.exist(newFile);
@@ -49,7 +54,7 @@ describe('gulp-beautify', function() {
 
         newFile.path.should.equal('/home/contra/test/file.js');
         newFile.relative.should.equal('file.js');
-        String(newFile.contents).should.equal(expected);
+        String(newFile.contents).should.equal(jsExpected);
         done();
       });
       stream.write(fakeFile);
@@ -57,26 +62,22 @@ describe('gulp-beautify', function() {
   });
 
   describe('beautify() – html', function() {
+    var htmlSource = createBuffer('<html><body><h1>foo</h1><h2>bar</h2></body></html>');
+    // don't use template string for Node < 6 support
+    var htmlExpected = '<html>\n\n<body>\n  <h1>foo</h1>\n  <h2>bar</h2>\n</body>\n\n</html>';
+
     var getHtmlFile = function(ext) {
       return new File({
-        path: `/home/contra/test/file.${ext}`,
+        path: '/home/contra/test/file.' + ext,
         base: '/home/contra/test/',
         cwd: '/home/contra/',
-        contents: new Buffer.from('<html><body><h1>foo</h1><h2>bar</h2></body></html>')
+        contents: htmlSource
       });
     };
-    var expected = `<html>
-
-<body>
-  <h1>foo</h1>
-  <h2>bar</h2>
-</body>
-
-</html>`;
 
     it('should beautify .html', function(done) {
       var stream = beautify.html({ indent_size: 2 });
-      var fakeFile = getHtmlFile(`html`);
+      var fakeFile = getHtmlFile('html');
 
       stream.on('error', done);
       stream.on('data', function(newFile) {
@@ -87,7 +88,7 @@ describe('gulp-beautify', function() {
 
         newFile.path.should.equal('/home/contra/test/file.html');
         newFile.relative.should.equal('file.html');
-        String(newFile.contents).should.equal(expected);
+        String(newFile.contents).should.equal(htmlExpected);
         done();
       });
       stream.write(fakeFile);
@@ -95,7 +96,7 @@ describe('gulp-beautify', function() {
 
     it('should beautify .htm', function(done) {
       var stream = beautify.html({ indent_size: 2 });
-      var fakeFile = getHtmlFile(`htm`);
+      var fakeFile = getHtmlFile('htm');
 
       stream.on('error', done);
       stream.on('data', function(newFile) {
@@ -106,7 +107,7 @@ describe('gulp-beautify', function() {
 
         newFile.path.should.equal('/home/contra/test/file.htm');
         newFile.relative.should.equal('file.htm');
-        String(newFile.contents).should.equal(expected);
+        String(newFile.contents).should.equal(htmlExpected);
         done();
       });
       stream.write(fakeFile);
@@ -114,18 +115,19 @@ describe('gulp-beautify', function() {
   });
 
   describe('beautify() – css', function() {
+    var cssSource = createBuffer('.foo { color: red; }');
+    // don't use template string for Node < 6 support
+    var cssExpected = '.foo {\n  color: red;\n}';
+
     it('should beautify .css', function(done) {
       var stream = beautify.css({ indent_size: 2 });
       var fakeFile = new File({
         path: '/home/contra/test/file.css',
         base: '/home/contra/test/',
         cwd: '/home/contra/',
-        contents: new Buffer.from(`.foo { color: red; }`)
+        contents: cssSource
       });
 
-      var expected = `.foo {
-  color: red;
-}`;
       stream.on('error', done);
       stream.on('data', function(newFile) {
         should.exist(newFile);
@@ -135,7 +137,7 @@ describe('gulp-beautify', function() {
 
         newFile.path.should.equal('/home/contra/test/file.css');
         newFile.relative.should.equal('file.css');
-        String(newFile.contents).should.equal(expected);
+        String(newFile.contents).should.equal(cssExpected);
         done();
       });
       stream.write(fakeFile);
