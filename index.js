6e52200ef3edf1699b805d02ed4268fac83aec40
Hiswe <hiswehalya@gmail.com>
2019-02-26 04:40:16 +0100

    Breaking: Add support for html/css beautification & remove rcloader  (#26)

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 var through = require('through2');
-var beautify = require('js-beautify').js_beautify;
-var RcLoader = require('rcloader');
+var beautify = require('js-beautify');
+var hasBufferFrom = typeof Buffer.from !== 'undefined'
 
-module.exports = function(opts) {
-  var rcLoader = new RcLoader('.jsbeautifyrc', opts, { loader: 'async' });
-
-  function modifyFile(file, enc, cb) {
-    if (file.isNull()) return cb(null, file); // pass along
-    if (file.isStream()) return cb(new Error('gulp-beautify: Streaming not supported'));
-    rcLoader.for(file.path, function (err, opts) {
-      if (err) return cb(err);
-
+function createBeautifyPlugin(beautifier) {
+  return function gulpBeautify(opts) {
+    function modifyFile(file, enc, cb) {
+      if (file.isNull()) return cb(null, file); // pass along
+      if (file.isStream()) return cb(new Error('gulp-beautify: Streaming not supported'));
       var str = file.contents.toString('utf8');
-      file.contents = new Buffer(beautify(str, opts));
+      var result = beautifier(str, opts)
+      // support Node version 0.10/0.12
+      // while preventing deprecation warning on newer versions 
+      file.contents = hasBufferFrom ? new Buffer.from(result) : new Buffer(result);
       cb(null, file);
-    });
-  }
+    }
+    return through.obj(modifyFile);
+  };
+}
 
-  return through.obj(modifyFile);
-};
+// keep the same exports as “js-beautify”
+var beautifyExport = createBeautifyPlugin(beautify);
+beautifyExport.js = beautifyExport;
+beautifyExport.html = createBeautifyPlugin(beautify.html);
+beautifyExport.css = createBeautifyPlugin(beautify.css);
+
+module.exports = beautifyExport;
--- test/index.js
+++ test/index.js
@@ -3,20 +3,26 @@ var should = require('should');
 var File = require('vinyl');
 require('mocha');
 
+function createBuffer(string) {
+  return Buffer.from ? new Buffer.from(string) : new Buffer(string);
+}
+
 describe('gulp-beautify', function() {
-  describe('beautify()', function() {
+  describe('beautify() – javascript', function() {
+    var jsSource = createBuffer('function test(){console.log("test");}');
+    var jsExpected = 'function test() {\n  console.log("test");\n}';
+
     it('should concat two files', function(done) {
-      var stream = beautify({indent_size: 2});
+      var stream = beautify({ indent_size: 2 });
       var fakeFile = new File({
         path: '/home/contra/test/file.js',
         base: '/home/contra/test/',
         cwd: '/home/contra/',
-        contents: new Buffer('function test(){console.log("test");}')
+        contents: jsSource
       });
 
-      var expected = "function test() {\n  console.log(\"test\");\n}";
       stream.on('error', done);
-      stream.on('data', function(newFile){
+      stream.on('data', function(newFile) {
         should.exist(newFile);
         should.exist(newFile.path);
         should.exist(newFile.relative);
@@ -24,7 +30,114 @@ describe('gulp-beautify', function() {
 
         newFile.path.should.equal('/home/contra/test/file.js');
         newFile.relative.should.equal('file.js');
-        String(newFile.contents).should.equal(expected);
+        String(newFile.contents).should.equal(jsExpected);
+        done();
+      });
+      stream.write(fakeFile);
+    });
+
+    it('should be the same with the js export', function(done) {
+      var stream = beautify.js({ indent_size: 2 });
+      var fakeFile = new File({
+        path: '/home/contra/test/file.js',
+        base: '/home/contra/test/',
+        cwd: '/home/contra/',
+        contents: jsSource
+      });
+
+      stream.on('error', done);
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+
+        newFile.path.should.equal('/home/contra/test/file.js');
+        newFile.relative.should.equal('file.js');
+        String(newFile.contents).should.equal(jsExpected);
+        done();
+      });
+      stream.write(fakeFile);
+    });
+  });
+
+  describe('beautify() – html', function() {
+    var htmlSource = createBuffer('<html><body><h1>foo</h1><h2>bar</h2></body></html>');
+    // don't use template string for Node < 6 support
+    var htmlExpected = '<html>\n\n<body>\n  <h1>foo</h1>\n  <h2>bar</h2>\n</body>\n\n</html>';
+
+    var getHtmlFile = function(ext) {
+      return new File({
+        path: '/home/contra/test/file.' + ext,
+        base: '/home/contra/test/',
+        cwd: '/home/contra/',
+        contents: htmlSource
+      });
+    };
+
+    it('should beautify .html', function(done) {
+      var stream = beautify.html({ indent_size: 2 });
+      var fakeFile = getHtmlFile('html');
+
+      stream.on('error', done);
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+
+        newFile.path.should.equal('/home/contra/test/file.html');
+        newFile.relative.should.equal('file.html');
+        String(newFile.contents).should.equal(htmlExpected);
+        done();
+      });
+      stream.write(fakeFile);
+    });
+
+    it('should beautify .htm', function(done) {
+      var stream = beautify.html({ indent_size: 2 });
+      var fakeFile = getHtmlFile('htm');
+
+      stream.on('error', done);
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+
+        newFile.path.should.equal('/home/contra/test/file.htm');
+        newFile.relative.should.equal('file.htm');
+        String(newFile.contents).should.equal(htmlExpected);
+        done();
+      });
+      stream.write(fakeFile);
+    });
+  });
+
+  describe('beautify() – css', function() {
+    var cssSource = createBuffer('.foo { color: red; }');
+    // don't use template string for Node < 6 support
+    var cssExpected = '.foo {\n  color: red;\n}';
+
+    it('should beautify .css', function(done) {
+      var stream = beautify.css({ indent_size: 2 });
+      var fakeFile = new File({
+        path: '/home/contra/test/file.css',
+        base: '/home/contra/test/',
+        cwd: '/home/contra/',
+        contents: cssSource
+      });
+
+      stream.on('error', done);
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+
+        newFile.path.should.equal('/home/contra/test/file.css');
+        newFile.relative.should.equal('file.css');
+        String(newFile.contents).should.equal(cssExpected);
         done();
       });
       stream.write(fakeFile);
